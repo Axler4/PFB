67006f7c1745d9419a27a6f369895b236ba3ad48
Axler4 <123861077+Axler4@users.noreply.github.com>
2023-01-29 23:56:14 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,1 +1,21 @@
-fp = Path.cwd()/"cash on hand csv"
+from pathlib import Path
+import matplotlib.pyplot as plt 
+import csv
+
+fp = Path.cwd()/"Cash_On_Hand.csv"
+
+sums = {}
+
+with open(fp) as file:
+    reader = csv.reader(file)
+    next(reader) # skip header row
+    for column in reader:
+        day, amount = column
+        if day in sums:
+            sums[day] += float(amount)
+        else:
+            sums[day] = float(amount)
+
+# Print the sums for each day
+for day, amount in sums.items():
+    print(f'{day}: ${amount}')
