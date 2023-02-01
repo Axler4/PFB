1a217465a4bffa0e26f6002d66ca89e1b813a26d
Kyrunic <123966866+Kyrunic@users.noreply.github.com>
2023-02-01 18:15:26 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -1,9 +1,18 @@
 import csv
+from pathlib import Path
 
 def overhead_max_percentage():
+    '''
+    setup csv reading functions to calculate percentage of highest overhead cost
+    by sorting each category name and add each amount to list
+    no required parameters
+    '''
+    
+    fp = Path.cwd()/ r"C:\Users\amias\IGP programming\csv_files\Overheads.csv"
+    # setup csv reading functions skipping the first row
     with open('overheads.csv', 'r') as file:
         reader = csv.reader(file) 
-        next(reader) # skip the header row
+        next(reader)
 
         # Lists to store expenses by category
         category = [['Depreciation Expense', 0.0],
@@ -18,13 +27,13 @@ def overhead_max_percentage():
                     ['Salary Expense', 0.0],
                     ['Shipping Expense', 0.0],
                     ['Total Expenses', 0.0]]    
-
+        # sets category as row 1 and amount as row 3
         for row in reader:
             category_name = row[1]
             amount = float(row[3]) 
-
+            # calculates total amount for all expenses
             category[11][1] += amount  
-
+            # sorts each expense category by name
             if category_name == 'Depreciation Expense':
                 category[0][1] += amount
 
@@ -57,13 +66,16 @@ def overhead_max_percentage():
 
             elif category_name == 'shipping Expense':
                 category[10][1] += amount 
-
+    # initialize highest overhead and category
     highest_overhead = 0
     highest_overhead_category = ""
+    # uses a for loop to calculate the highest overhead
+    # makes a for loop for all categories excluding total expense 
     for idx in category[:len(category)-1]:
         if idx[1] > highest_overhead:
             highest_overhead = idx[1]
             highest_overhead_category = idx[0]
+    # calculates the percentage of the highest overhead
     highest_overhead_percentage = highest_overhead / category[11][1] * 100
 
     return highest_overhead_percentage, highest_overhead_category
